12b371869abfcda15c268002d6ea165cb17f2080
vmihaylov <vasiliy.mikhailov@gmail.com>
2022-12-21 16:56:08 +0300

    year 2023 quarters

--- server/second_eye_api/migrate_from_external_db/extract/quarters_extractor.py
+++ server/second_eye_api/migrate_from_external_db/extract/quarters_extractor.py
@@ -8,6 +8,10 @@ class QuartersExtractor:
             [ 20222, "2022-II", 2022, 2 ],
             [ 20223, "2022-III", 2022, 3 ],
             [ 20224, "2022-IV", 2022, 4 ],
+            [ 20231, "2023-I", 2023, 1],
+            [ 20232, "2023-II", 2023, 2],
+            [ 20233, "2023-III", 2023, 3],
+            [ 20234, "2023-IV", 2023, 4],
         ]
 
         self.data = pd.DataFrame(quarters_array, columns=["id", "name", "year", "quarter_number"])
