8d59700b4df1465ff10793a7a441935fe1aade9d
vmihaylov <vasiliy.mikhailov@gmail.com>
2022-12-21 18:40:09 +0300

    build

--- client/build-win-i2.cmd
+++ client/build-win-i2.cmd
@@ -1,2 +1,3 @@
 npm install -S react-scripts
-set "REACT_APP_SECOND_EYE_API_URL=http://kkt-app-preprod:3000/graphql/" && npm run build
+set REACT_APP_SECOND_EYE_API_URL="http://kkt-app-preprod:3000/graphql/"
+npm run build
